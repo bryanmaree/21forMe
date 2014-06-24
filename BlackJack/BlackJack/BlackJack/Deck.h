2fdbc8d033c9d2166331bfc514fe3a889fd403bb
Bryan Wilson <bryanmaree@gmail.com>
2014-06-24 13:45:33 -0600

    here is the code that was changed

--- BlackJack/BlackJack/BlackJack/Deck.h
+++ BlackJack/BlackJack/BlackJack/Deck.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "Card.h"
 #include <array>
 #include <random>
 #include <algorithm>
@@ -6,26 +7,109 @@
 #include <time.h>
 using namespace std;
 
-class Deck
-{
+//------------------------Deck Class.h---------------------------------
+class Deck {
 private:
-	char* deck;
+	static const int DECKSIZE = 52;
+	static const int FACES = 13;
+	static const int SUITS = 4;
+
+	Card cards[DECKSIZE];
+
 	int cardsUsed;
+
 public:
-	static const int deckCount = 52;
 	Deck(void);
-	
 	~Deck(void);
+	void clear();
 
-	//Hit Function
-	//Purpose to draw a card from the Deck and increment the cardsUsed
-	char Hit();
-
-	//Suffle Function:
-	//Purpose, take the deck and shuffle it so that that its random
-	//parameters: n/a
-	//side effects:n/a
+	//Draw a card from the deck and increment the cardsUsed.
+	Card& hit();
+	//Shuffle the deck so that that it is random.
 	void shuffleIt();
+
 	void print();
+
+	int getCardsUsed() { return cardsUsed; }
+
+	bool isCardUsed(int location) { return cards[location].used; }
+
+	int getCardValue( int location );
 };
 
+//------------------------Deck Class.cpp---------------------------------
+Deck::Deck(void) {
+	
+	clear();
+}
+
+Deck::~Deck(void) {}
+
+void Deck::clear() {
+
+	for( int i=0; i<SUITS; i++ )
+	{
+		for( int j=1; j<=FACES; j++ )
+		{
+			cards [ i*FACES + j -1 ] = Card(j,i);
+		}
+	}
+
+	cardsUsed = 0;
+	shuffleIt();
+}
+
+Card& Deck::hit() {
+	if( cardsUsed < DECKSIZE ) {
+		return *cards[cardsUsed++].use();
+	}
+
+	else throw string("Deck overflow");
+}
+
+void Deck::shuffleIt() {
+	//Create a temporary deck to put cards in a random order.
+	Card tempDeck[DECKSIZE];
+
+	srand(time(NULL));
+
+	//here comes the shuffle
+	for(int i = 0; i < DECKSIZE; i++) {
+		//make spot be a random remainder of the deck count 52
+		int spot = rand() % DECKSIZE;
+		bool loop = true;
+		do {
+			// If the spot is open put the deck[i] in that spot
+			if(tempDeck[spot].face == 0) {
+				tempDeck[spot] = cards[i];
+				//do not loop
+				loop = false;
+			} else {
+				//because spot wasn"t open, just move one spot. 
+				//or if the spot has gone past the end of the deck loop back arround to 0
+				spot = (spot+1) % DECKSIZE;
+				loop = true;
+			}
+		}while(loop);
+	}
+
+	//Point the deck to the tempDeck we just created.
+	for ( int i=0; i<DECKSIZE; i++)
+	{
+		cards[i] = tempDeck[i];
+	}
+}
+
+void Deck::print() {
+	for(int i = 0; i< DECKSIZE; i++) {
+		
+		cout << cards[i] << endl;
+
+	}
+}
+
+int Deck::getCardValue(int location)
+{
+	if ( cards[location].face > 9 ) return 10;
+	else return cards[location].face;
+}
--- BlackJack/BlackJack/BlackJack/Player.h
+++ BlackJack/BlackJack/BlackJack/Player.h
@@ -1,17 +1,82 @@
 #pragma once
+#include "Card.h"
 #include <iostream>
+
 using namespace std;
-class Player
-{
-private:
-	char hand[5];
+
+char choice;
+//------------------------Player Class.h---------------------------------
+class Player {
+protected:
+	static const int MAXCARDS = 11;
+	static const int BLACKJACK = 21;
+	Card hand[MAXCARDS];
 	int cardNumber;
+	bool aced;
 public:
-	Player(void);
+	Player();
 	~Player(void);
-	void hit(char t);
+	void clear();
+	
+	void hit(const Card& c);
 	void printHand();
 	int handTotal();
-
+	bool hasAce() { return aced; }
 };
 
+//------------------------Player Class.cpp---------------------------------
+Player::Player()
+{
+	clear();
+}
+
+Player::~Player(void)
+{
+}
+
+void Player::clear() {
+	for(int i = 0; i <MAXCARDS; i++) {
+		hand[i] = Card();
+	}
+	cardNumber = 0;
+	aced = false;
+}
+
+void Player::hit(const Card& c) {
+	hand[cardNumber++] = c;
+	if ( c.face == 1 ) aced = true;
+}
+
+void Player::printHand() {
+
+	for(int i = 0; i < cardNumber; i++) {
+			cout << hand[i] << ", ";
+	}
+
+	int hand = handTotal();
+
+
+	if ( aced && hand+10 <= BLACKJACK ) hand += 10;
+
+	if(handTotal() == BLACKJACK) {
+		cout << "\nTotal: " << hand << ", Blackjack!!" << endl;
+	} else if(handTotal() > BLACKJACK) {
+		cout << "\nTotal: " << hand << ", Bust!!" << endl;
+	} else {
+		cout << "\nTotal: " << hand << endl;
+	}
+}
+
+int Player::handTotal() 
+{
+	int handTotal = 0;
+
+	for(int i = 0; i < cardNumber; i++) {
+		if(hand[i].face > 10 ) {
+			handTotal += 10;
+		} else {
+			handTotal += hand[i].face;
+		}
+	}
+	return handTotal;
+}
